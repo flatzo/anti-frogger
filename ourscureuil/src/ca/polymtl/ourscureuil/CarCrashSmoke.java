a7d8eb548e9053467b520c5abfd4f50eb2a17e23
thierry.belair@polymtl.ca <thierry.belair@polymtl.ca@05535eda-d6a5-4ac4-b685-4147b9d08a8c>
2012-09-30 04:24:17 +0000

    nuage de fumee
    git-svn-id: https://svn.aep.polymtl.ca/svn/GCA3OursCureuil@65 05535eda-d6a5-4ac4-b685-4147b9d08a8c

---
+++ ourscureuil/src/ca/polymtl/ourscureuil/CarCrashSmoke.java
@@ -0,0 +1,62 @@
+package ca.polymtl.ourscureuil;
+ 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveBy;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveTo;
+import com.badlogic.gdx.scenes.scene2d.actions.Repeat;
+import com.badlogic.gdx.scenes.scene2d.actions.RotateTo;
+import com.badlogic.gdx.scenes.scene2d.actions.Sequence;
+
+public class CarCrashSmoke extends Node {
+	
+	static final float HITBOX_WIDTH = 48.0f;
+	static final float HITBOX_HEIGHT = 48.0f;
+	
+	public CarCrashSmoke(Vector2 posStart, String nameOfNode) { //position start is left, bottom corner
+		super(posStart, nameOfNode);
+		this.height = HITBOX_HEIGHT;
+		this.width  = HITBOX_WIDTH;
+		// TODO Auto-generated constructor stub
+	}
+	
+	protected static final Texture projectileTextureList = new Texture(Gdx.files.internal("data/vehicles_512.png"));
+	
+	//x = 96+48=144
+	// y = 144
+	// w = 48
+	// h = 48
+	//static protected TextureRegion texreg = new TextureRegion(projectileTextureList, 96, 144, 48, 48);	
+	//static protected TextureRegion texregDead = new TextureRegion(projectileTextureList, 48, 144, 48, 48); //grenouille
+	//static protected TextureRegion texregDead = new TextureRegion(projectileTextureList, 72, 144, 48, 48); //grenouille demie
+	static protected TextureRegion texregDead = new TextureRegion(projectileTextureList, 144, 144, 48, 48); //grenouille morte 
+	
+	public void SetMovement(Vector2 movement) {
+		//car crash smoke does not move
+		
+		//this.action(Repeat.$(Sequence.$(MoveBy.$(movement.x, movement.y, 1), MoveBy.$(-movement.x, movement.y, 1)),5));
+	}
+
+	@Override
+	public void draw(SpriteBatch batch, float parentAlpha) {
+		
+		//System.out.println("(" + Double.toString(x) + "," + Double.toString(y) + ")");
+		
+		
+		batch.draw(texregDead,this.originX+this.x,this.originY+this.y,HITBOX_WIDTH/2,HITBOX_HEIGHT/2,HITBOX_WIDTH,HITBOX_HEIGHT,1.0f,1.0f,this.rotation);
+	}
+
+	@Override
+	public Actor hit(float x, float y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
